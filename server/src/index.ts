b26b1901f4cb328d33e9f459cbe9af4f5be109dc
Eliott <eliott.poulain@epitech.eu>
2024-09-15 15:34:01 +0200

    install new local db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
+import {initialiseDatabase} from "./database/database";
 
 const app = express();
-const port = 8000;
-
+initialiseDatabase();
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello TypeScript with Express!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(8000, () => {
+    console.log(`Server is running at http://localhost:8000`);
 });
